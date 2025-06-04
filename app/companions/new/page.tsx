a9257e264bff9340040122b502466538c73a7712
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-06-04 15:47:07 +0530

    create new page

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,10 +1,14 @@
+import CompanionFrom from '@/components/CompanionFrom'
 import React from 'react'
 
 const NewCompanions = () => {
   return (
-    <div>
-
-    </div>
+    <main className='min-lg:w-1/3 min-md:w-2/3 items-center justify-center'>
+      <article className='w-full gap-4 flex flex-col'>
+        <h1>Comapanion builder</h1>
+        <CompanionFrom />
+      </article>
+    </main>
   )
 }
 
