3698337a9ba3c628706606e2421366aa94fccacf
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-06-04 07:46:03 +0530

    setup navbar to home page

---
+++ components/Navbar.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import NavbarItems from './NavbarItems'
+
+const Navbar = () => {
+  return (
+    <nav className='flex items-center justify-between mx-auto w-full px-14 py-4 bg-white max-sm:px-4'>
+      <Link href='/'>
+        <div className="flex items-center gap-2.5 cursor-pointer">
+          <Image
+            src='/images/logo.svg'
+            alt='logo'
+            width={46}
+            height={44}
+          />
+        </div>
+      </Link>
+
+      <div className="flex items-center gap-8">
+        <NavbarItems />
+      </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
